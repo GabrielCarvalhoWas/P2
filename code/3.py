c1eb416eb8bf201502c1f39ee942f40915f3f26c
Gabriel <pereira.carvalho@academico.ifpb.edu.br>
2024-11-08 14:34:07 -0300

    Update 3.py

--- code/3.py
+++ code/3.py
@@ -8,3 +8,12 @@
 # Método que adiciona o nome de país, a lista de fronteiras(verificar se o nome já existe na lista).
 # Método __str__(self).
 
+
+class Pais:
+    def __init__(self, nome: str, nome_Capital: str, km: float, paises_fronteira: list = []):
+        self.__nome = nome
+        self.__nome_Capital = nome_Capital
+        self.__km = km
+    
+    def adad():
+        ...
