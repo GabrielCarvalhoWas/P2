ca85958c24b2ac8ad0d6096a3edcc68041e438a9
Gabriel <pereira.carvalho@academico.ifpb.edu.br>
2024-11-08 17:28:20 +0000

    Aula09

---
+++ code/3.py
@@ -0,0 +1,10 @@
+# 3. Escreva uma classe que represente um país. Um país tem como atributos privados o seu nome,
+#  o nome da capital, sua dimensão em Km2 e uma lista de países com os quais ele faz
+
+# fronteira. Represente a classe e forneça os seguintes construtores e método:
+# Construtor que inicialize o nome, capital e a dimensão do país;
+# Métodos de acesso para os atributos indicados no item (a);
+# Método que retorne a lista de países que fazem fronteira;
+# Método que adiciona o nome de país, a lista de fronteiras(verificar se o nome já existe na lista).
+# Método __str__(self).
+
